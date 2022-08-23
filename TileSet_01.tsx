782b6b0083ba9b05c34d6aac2fe8f684c4b4135b
KaiBit <s0577375@HTW-Berlin.de>
2022-08-23 22:17:13 +0200

    New Tileset

---
+++ TileSet_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="TileSet_01" tilewidth="32" tileheight="32" tilecount="42" columns="6">
+ <image source="tilesets/TileSet_01.png" trans="000000" width="192" height="224"/>
+</tileset>
